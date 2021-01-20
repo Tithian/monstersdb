eb39ea1cef681a740dca650fecaaeaaebb8fa8be
Tithian <31572816+Tithian@users.noreply.github.com>
2021-01-20 21:16:08 +0100

    21.15 20/01/21
    asfasasdf

---
+++ monstersdb/web_dumper/dandwiki_dumper.py
@@ -0,0 +1,13 @@
+import os
+
+import requests
+
+from monstersdb.web_parsers.dandwiki import ListCreaturesHTMLParser, parse_url
+
+def dump_all(folder):
+    pass
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- monstersdb/web_parsers/dandwiki.py
+++ monstersdb/web_parsers/dandwiki.py
@@ -13,34 +13,39 @@ class SRD35_HTMLParser(HTMLParser):
         self.monster = []
         self.table = {}
         self.content = {}
+        self.description = {}
         # Flags
         self.in_table = False
         self.in_key = False
         self.in_value = False
-        self.in_content = False
-        self.in_key_content = False
-        self.in_value_content = False
+        self.in_combate = False
         self.in_p = False
+        self.in_span = False
+        self.start_description = False
+        self.in_description = False
         self.finish = False
         # Temps
         self.temp_table = {}
-        self.temp_content = {}
         self.temp_key = ''
         self.temp_value = ''
-        self.temp_key_content = ''
-        self.temp_value_content = ''
+        self.temp_combate = ''
+        self.temp_description = ''
 
     def reset(self):
         super(SRD35_HTMLParser, self).reset()
         self.monster = []
         self.table = {}
         self.content = {}
+        self.description = {}
         self.in_table = False
         self.in_key = False
         self.in_value = False
-        self.in_key_content = False
-        self.in_value_content = False
+        self.in_combate = False
         self.in_p = False
+        self.in_span = False
+        self.start_description = False
+        self.in_description = False
+        self.finish = False
 
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
@@ -50,48 +55,98 @@ class SRD35_HTMLParser(HTMLParser):
             self.temp_table = {}
             self.in_table = True
 
-        elif self.in_table and tag == 'th':
-            self.temp_key = ''
-            self.in_key = True
+        elif self.in_table:
+            if tag == 'th':
+                self.temp_key = ''
+                self.in_key = True
+            if tag == 'td':
+                self.temp_value = ''
+                self.in_value = True
 
-        elif self.in_table and tag == 'td':
-            self.temp_value = ''
-            self.in_value = True
-
-        elif tag == 'table' and css == 'd20':
-            self.in_content = False
+        elif tag == 'h1' and 'firstHeading' in css:
+            self.start_description = True
 
         elif tag == 'span' and 'headline' in css:
             css = attrs.get('id', '')
+            self.start_description = True
+
             if tag == 'span' and 'COMBAT' in css:
-                self.in_content = True
+                self.in_combate = True
+                self.in_description = False
+            elif tag == 'span' and 'Combat' in css:
+                self.in_combate = True
+                self.in_description = False
             elif tag == 'span' and 'Also' in css:
-                self.in_content = False
+                self.in_description = False
+                self.in_combate = False
+                self.finish = True
+            elif tag == 'span' and 'Characters' in css:
+                self.in_description = False
+                self.in_combate = False
                 self.finish = True
 
-        elif self.in_content and tag == 'p':
+        elif tag == 'a':
+            title = attrs.get('title', '')
+            if 'Main Page' in title:
+                self.finish = True
+                self.in_description = False
+                self.in_combate = False
+                self.start_description = False
+
+        elif tag == 'caption':
+            self.in_description = False
+            self.finish = True
+            self.in_combate = False
+
+        elif self.start_description and tag == 'p':
+            self.in_description = True
+
+
+
+        elif self.in_combate and tag == 'p':
             self.in_p = True
 
     def handle_endtag(self, tag):
-        if self.in_table and tag == 'table':
-            self.in_table = False
+        if self.in_table:
+            if tag == 'table':
+                self.in_table = False
+                self.monster.append(self.temp_table)
+                self.monster.append(self.description)
+                self.monster.append(self.content)
+                self.temp_table = {}
+                self.temp_description = ''
+                self.temp_combate = ''
 
-        elif self.in_key and tag == 'th':
-            self.in_key = False
+            elif self.in_key and tag == 'th':
+                self.in_key = False
 
-        elif self.in_value and tag == 'td':
-            self.in_value = False
+            elif self.in_value and tag == 'td':
+                self.in_value = False
 
-        elif tag == 'tr':
-            if self.temp_key.endswith(':'):
-                self.temp_key = self.temp_key[:-1]
-            self.table[self.temp_key] = self.temp_value
+            elif tag == 'tr':
+                if self.temp_key.endswith(':'):
+                    self.temp_key = self.temp_key[:-1]
+                self.temp_table[self.temp_key] = self.temp_value
+                self.temp_key = ''
+                self.temp_value = ''
 
-        elif self.in_content and tag == 'p':
+        elif self.start_description and tag == 'p':
+            self.temp_description.replace('Back to Main Page → 3.5e Open Game Content → '
+                                          'System Reference Document → Creatures', '')
+            if not self.description:
+                if self.temp_description:
+                    self.description['Descripción'] = self.temp_description
+
+        elif self.in_combate and tag == 'p':
             self.in_p = False
 
+
         elif self.finish:
-            self.content[self.temp_key_content] = self.temp_value_content
+            self.temp_combate = self.temp_combate.replace('COMBAT', '')
+            self.temp_combate = self.temp_combate.replace('Combat', '')
+            if not self.content:
+                self.content['Combate'] = self.temp_combate
+
 
     def handle_data(self, data):
         data = data.replace('\n', '')
@@ -102,11 +157,13 @@ class SRD35_HTMLParser(HTMLParser):
         elif self.in_table and self.in_value:
             self.temp_value += data
 
-        elif not self.in_p and self.in_content:
-            self.temp_key_content +=data
+        elif not self.in_p and self.in_combate:
+            self.temp_combate +=data
 
-        elif self.in_p:
-            self.temp_value_content += data
+        elif self.in_description:
+            self.temp_description += data
+
+
 
 class ListCreaturesHTMLParser(HTMLParser):
     def __init__(self, *arg, **kwargs):
@@ -127,6 +184,7 @@ class ListCreaturesHTMLParser(HTMLParser):
                 self.start = True
 
 
+
 def parse_url(parser, url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -134,15 +192,18 @@ def parse_url(parser, url):
 
 
 def main():
-    srd_parser = SRD35_HTMLParser()
-    creature_url = 'https://www.dandwiki.com/wiki/SRD:Air_Elemental'
-    print(creature_url)
-    parse_url(srd_parser, creature_url)
-    srd_parser.table.pop('', '')
-    print(srd_parser.table)
-    print(srd_parser.content)
-    print(srd_parser.monster)
+    list_creatures_parser = ListCreaturesHTMLParser()
+    parse_url(list_creatures_parser, 'https://www.dandwiki.com/wiki/Creatures')
 
+    srd_parser = SRD35_HTMLParser()
+    for creature_path in sorted(list_creatures_parser.list_creatures):
+        creature_url = 'https://www.dandwiki.com{path}'.format(path=creature_path)
+        print(creature_url)
+        parse_url(srd_parser, creature_url)
+        srd_parser.table.pop('', '')
+        for monster in srd_parser.monster:
+            print(monster)
+        srd_parser.reset()
 
 
 if __name__ == '__main__':
